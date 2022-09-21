50d0228a610c1ca7982096472bf4025b7be7e262
Bruno Braga <braga.bruno81@gmail.com>
2022-09-21 12:46:44 -0500

    typo fixed

--- index.js
+++ index.js
@@ -1,8 +1,38 @@
-const PORT = 800
+const hostname = '127.0.0.1'
+
+const PORT = 8000
 const express = require('express')
 const axios = require('axios')
 const cheerio = require('cheerio')
+const {response, json} = require("express");
 
 const app = express()
 
-app.listen((PORT, () => console.log(`server running on PORT: ${PORT}`)))
+const articles = []
+
+app.get('/', (req, res) => {
+    res.json('Welcome to my Climate Change News API')
+})
+
+app.get('/news', (req, res) => {
+    axios.get('https://www.theguardian.com/environment/climate-crisis')
+        .then((response) => {
+            const html = response.data
+            console.log(html)
+            const $ = cheerio.load(html)
+
+            $('a:contains("climate")', html).each(function() {
+                const title = $(this).text()
+                const url = $(this).attr('href')
+                articles.push({
+                    title,
+                    url
+                })
+            })
+            res.json(articles)
+        }).catch((err) => console.log(err))
+})
+
+app.listen(PORT, hostname,() => {
+    console.log(`Server running at http://${hostname}:${PORT}/`)
+})
